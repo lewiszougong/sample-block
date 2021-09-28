5abf83bd75ecc61709896349ca16b6347f0c421f
DIR\wah.wai.kwong <wah.wai.kwong@accenture.com>
2021-09-28 16:58:47 +0800

    test remove address

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ require('../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-l
 var SDK = require('blocksdk');
 var sdk = new SDK(null, null, true); // 3rd argument true bypassing https requirement: not prod worthy
 
-var address, width, height, zoom, link, mapsKey;
+var width, height, zoom, link, mapsKey;
 
 function debounce (func, wait, immediate) {
 	var timeout;
@@ -22,7 +22,6 @@ function debounce (func, wait, immediate) {
 
 function paintSettings () {
 	document.getElementById('text-input-id-0').value = mapsKey;
-	document.getElementById('text-input-id-1').value = address;
 	document.getElementById('slider-id-01').value = width;
 	document.getElementById('slider-id-02').value = height;
 	document.getElementById('slider-id-03').value = zoom;
@@ -36,20 +35,16 @@ function paintSliderValues () {
 
 function paintMap() {
 	mapsKey = document.getElementById('text-input-id-0').value;
-	address = document.getElementById('text-input-id-1').value;
 	width = document.getElementById('slider-id-01').value;
 	height = document.getElementById('slider-id-02').value;
 	zoom = document.getElementById('slider-id-03').value;
 	link = document.getElementById('text-input-id-2').value;
-	if (!address) {
-		return;
-	}
-	var url = 'https://maps.googleapis.com/maps/api/staticmap?center=' +
-		address.split(' ').join('+') + '&size=' + width + 'x' + height + '&zoom=' + zoom +
-		'&markers=' + address.split(' ').join('+') + '&key=' + mapsKey;
+
+//	var url = 'https://maps.googleapis.com/maps/api/staticmap?center=' +
+//		address.split(' ').join('+') + '&size=' + width + 'x' + height + '&zoom=' + zoom +
+//		'&markers=' + address.split(' ').join('+') + '&key=' + mapsKey;
 	sdk.setContent(mapsKey);
 	sdk.setData({
-		address: address,
 		width: width,
 		height: height,
 		zoom: zoom,
@@ -60,7 +55,6 @@ function paintMap() {
 }
 
 sdk.getData(function (data) {
-	address = data.address || '';
 	width = data.width || 400;
 	height = data.height || 300;
 	zoom = data.zoom || 15;
