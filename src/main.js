5d209d395629d8df14f3dce66ae3d47728be88d9
DIR\wah.wai.kwong <wah.wai.kwong@accenture.com>
2021-09-28 17:03:14 +0800

    test remove width

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ require('../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-l
 var SDK = require('blocksdk');
 var sdk = new SDK(null, null, true); // 3rd argument true bypassing https requirement: not prod worthy
 
-var width, height, zoom, link, mapsKey;
+var height, zoom, link, mapsKey;
 
 function debounce (func, wait, immediate) {
 	var timeout;
@@ -22,20 +22,17 @@ function debounce (func, wait, immediate) {
 
 function paintSettings () {
 	document.getElementById('text-input-id-0').value = mapsKey;
-	document.getElementById('slider-id-01').value = width;
 	document.getElementById('slider-id-02').value = height;
 	document.getElementById('slider-id-03').value = zoom;
 }
 
 function paintSliderValues () {
-	document.getElementById('slider-id-01-val').innerHTML = document.getElementById('slider-id-01').value;
 	document.getElementById('slider-id-02-val').innerHTML = document.getElementById('slider-id-02').value;
 	document.getElementById('slider-id-03-val').innerHTML = document.getElementById('slider-id-03').value;
 }
 
 function paintMap() {
 	mapsKey = document.getElementById('text-input-id-0').value;
-	width = document.getElementById('slider-id-01').value;
 	height = document.getElementById('slider-id-02').value;
 	zoom = document.getElementById('slider-id-03').value;
 	link = document.getElementById('text-input-id-2').value;
@@ -45,7 +42,6 @@ function paintMap() {
 //		'&markers=' + address.split(' ').join('+') + '&key=' + mapsKey;
 	sdk.setContent(mapsKey);
 	sdk.setData({
-		width: width,
 		height: height,
 		zoom: zoom,
 		link: link,
@@ -55,7 +51,6 @@ function paintMap() {
 }
 
 sdk.getData(function (data) {
-	width = data.width || 400;
 	height = data.height || 300;
 	zoom = data.zoom || 15;
 	link = data.link || '';
