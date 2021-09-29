27d1486e0d526f390dcd2892a258802d789178f9
niawjunior <niawkung@gmail.com>
2021-09-29 11:36:47 +0700

    update map key

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+var SDK = require("blocksdk");
+
+var sdk = new SDK(null, null, true); // 3rd argument true bypassing https requirement: not prod worthy
+
+var mapsKey;
+
 // Wait for the document to load before we doing anything
 document.addEventListener("DOMContentLoaded", function main() {
   // setup our ui event handlers
@@ -112,5 +118,49 @@ function onSelectedFile() {
   const findFile = TermAndConditionConsent.find((item) => item.id == fileId);
   fileStatus.textContent = findFile.Name;
 
+  console.log(findFile);
+
   $("#exampleModal").modal("hide");
+  mapsKey = findFile.Consent;
+
+  debounce(paintMap, 500)();
+  paintSliderValues();
 }
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function paintSettings() {
+  //document.getElementById('text-input-id-0').value = mapsKey;
+}
+
+function paintSliderValues() {}
+
+function paintMap() {
+  console.log(mapsKey);
+  sdk.setContent(mapsKey);
+  sdk.setData({
+    mapsKey: mapsKey,
+  });
+  localStorage.setItem("consent", mapsKey);
+}
+
+sdk.getData(function (data) {
+  mapsKey = data.mapsKey || "";
+  paintSettings();
+  paintSliderValues();
+  paintMap();
+});
